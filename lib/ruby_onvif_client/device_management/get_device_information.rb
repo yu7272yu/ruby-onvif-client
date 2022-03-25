b8e354ba365322e12eb553a4c19fa820a940ffcf
liyujie <1515374438@qq.com>
2022-03-25 16:11:31 +0800

    f

--- lib/ruby_onvif_client/device_management/get_device_information.rb
+++ lib/ruby_onvif_client/device_management/get_device_information.rb
@@ -11,15 +11,14 @@ module ONVIF
                 send_message message do |success, result|
                     if success
                         xml_doc = Nokogiri::XML(result[:content])
-                        puts("info=====>>>>", xml_doc.to_s)
+                        puts("info=====>>>>start", xml_doc.to_s)
+                        puts("info=====>>>>end")
                         info = {
                             mf: value(xml_doc, '//tds:Manufacturer'),
                             model: value(xml_doc, '//tds:Model'),
                             firmware_version: value(xml_doc, '//tds:FirmwareVersion'),
                             serial_number: value(xml_doc, '//tds:SerialNumber'),
-                            hardware_id: value(xml_doc, '//tds:HardwareId'),
-                            # Body: value(xml_doc, '//env:Body'),
-                            all: value(xml_doc, xml_doc.to_s)
+                            hardware_id: value(xml_doc, '//tds:HardwareId')
                         }
                         callback cb, success, info
                     else
